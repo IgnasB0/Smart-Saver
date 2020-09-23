2030ce7c2262a2e5d6e288ebe570ea9d89ff84d4
IgnasB <i.bujevicius@gmail.com>
2020-09-23 21:49:19 +0300

    Išlaidos įvedimas, pavadinimo išvedimas
    Išlaidos įvedimo forma ir išlaidos pavadinimo išvedimas

---
+++ Smart Saver/InputExpense.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Smart_Saver
+{
+    partial class InputExpense
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.expenseName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.expenseAmount = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.category = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.expenseAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // expenseName
+            // 
+            this.expenseName.Location = new System.Drawing.Point(346, 95);
+            this.expenseName.Name = "expenseName";
+            this.expenseName.Size = new System.Drawing.Size(214, 23);
+            this.expenseName.TabIndex = 0;
+            this.expenseName.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Expense Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // expenseAmount
+            // 
+            this.expenseAmount.DecimalPlaces = 2;
+            this.expenseAmount.Location = new System.Drawing.Point(346, 142);
+            this.expenseAmount.Name = "expenseAmount";
+            this.expenseAmount.Size = new System.Drawing.Size(214, 23);
+            this.expenseAmount.TabIndex = 2;
+            this.expenseAmount.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Amount";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // category
+            // 
+            this.category.FormattingEnabled = true;
+            this.category.Items.AddRange(new object[] {
+            "Transportation",
+            "Food",
+            "Health",
+            "Entertainment"});
+            this.category.Location = new System.Drawing.Point(346, 187);
+            this.category.Name = "category";
+            this.category.Size = new System.Drawing.Size(214, 23);
+            this.category.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Category";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(346, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Input new expense";
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(346, 237);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(214, 23);
+            this.date.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(255, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Date";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(406, 296);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 9;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(346, 346);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(214, 82);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(255, 349);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Your expense";
+            // 
+            // InputExpense
+            // 
+            this.AccessibleName = "Name";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.category);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.expenseAmount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.expenseName);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "InputExpense";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.expenseAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox expenseName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown expenseAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox category;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Smart Saver/InputExpense.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smart_Saver
+{
+
+    public partial class InputExpense : Form
+    {
+        public InputExpense()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText(expenseName.Text + '\n');
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+        }
+    }
+}
