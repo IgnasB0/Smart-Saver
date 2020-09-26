a499f62ba9dcb51008b95f5835dbcc4dc05e1d8d
IgnasB <i.bujevicius@gmail.com>
2020-09-26 21:03:55 +0300

    išlaidų, pajamų ir balanso skaičiavimas

--- Smart Saver/Program.cs
+++ Smart Saver/Program.cs
@@ -17,7 +17,7 @@ namespace Smart_Saver
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new InputExpense());
+            Application.Run(new SavingsDepositRepresentation());
         }
     }
 }
