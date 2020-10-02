b6d3e14e8e36af1d7c7da24cf63f35fc4e599982
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2020-10-02 15:14:07 +0300

    Viskas NewStuff.txt fale

---
+++ Smart Saver/DBmanager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+
+//static class DBmanager - DataBase manager
+
+namespace Smart_Saver
+{
+    static class DBmanager
+    {
+        public static void AddUserToDB(User user)
+        {
+            try
+            {
+                userDBFileWriter.WriteLine("" + user.Id + "," + user.FirstNames + "," + user.Surname + "," + user.Email + "," + user.Income);
+                userDBFileWriter.Flush();
+            }
+            catch (Exception e)
+            {
+                Logger.Log(e.ToString());
+            }
+        }
+        public static void ClearUserDB()
+        {
+            try
+            {
+                File.Delete(userDBFilePath);
+                File.Create(userDBFilePath);
+            }
+            catch (Exception e)
+            {
+                Logger.Log(e.ToString());
+            }
+        }
+        public static User GetUserById(int id)
+        {
+            try
+            {
+                List<string> users = new List<string>();
+                User user = null;
+                users = File.ReadAllLines(userDBFilePath).ToList();
+                foreach (string userData in users)
+                {
+                    string[] currentUser = userData.Split(',');
+                    if (int.Parse(currentUser[0]) == id)
+                    {
+                        user = new User(id, currentUser[1], currentUser[2], currentUser[3], int.Parse(currentUser[4]));
+                        return user;
+                    }
+                }
+                throw new Exception("Specified user cannot be found. Make sure written ID is correct");
+            }
+            catch (Exception e)
+            {
+                Logger.Log(e.ToString());
+                return null;
+            }
+
+        }
+        private static readonly string userDBFilePath = "..\\..\\..\\UserDB.csv";
+        private static readonly StreamWriter userDBFileWriter = new StreamWriter(userDBFilePath);
+    }
+}
---
+++ Smart Saver/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Smart_Saver
+{
+    static class Logger
+    {
+        public static void Log(string message)
+        {
+            logFileWriter.WriteLine(DateTime.Now +"|:   " + message);
+            logFileWriter.Flush();
+        }
+
+        private static readonly StreamWriter logFileWriter = new StreamWriter("..\\..\\..\\log.txt");
+    }
+}
