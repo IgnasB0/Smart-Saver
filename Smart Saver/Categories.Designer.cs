87b00d061550f1066c47562391520fca8cb7b3c0
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2020-11-05 22:17:43 +0200

    random error

--- Smart Saver/Categories.Designer.cs
+++ Smart Saver/Categories.Designer.cs
@@ -45,6 +45,7 @@
             this.label1.Size = new System.Drawing.Size(233, 30);
             this.label1.TabIndex = 0;
             this.label1.Text = "Add Expense Category";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
--- Smart Saver/Categories.cs
+++ Smart Saver/Categories.cs
@@ -50,5 +50,10 @@ namespace Smart_Saver
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
