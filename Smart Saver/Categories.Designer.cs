a7adfbd44d6fb0ba9704abef1db8b2d8c8d0e5a6
Levonaitė Auksė <aukse.levonaite@mif.stud.vu.lt>
2020-11-05 21:32:28 +0200

    Deleted button, fixed some bugs

--- Smart Saver/Categories.Designer.cs
+++ Smart Saver/Categories.Designer.cs
@@ -32,7 +32,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.CategoryName = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -40,57 +39,43 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(95, 7);
+            this.label1.Location = new System.Drawing.Point(109, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(233, 30);
+            this.label1.Size = new System.Drawing.Size(295, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Add Expense Category";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 61);
+            this.label2.Location = new System.Drawing.Point(11, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.Size = new System.Drawing.Size(113, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Category Name";
             // 
             // CategoryName
             // 
-            this.CategoryName.Location = new System.Drawing.Point(122, 58);
-            this.CategoryName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CategoryName.Location = new System.Drawing.Point(139, 77);
             this.CategoryName.Name = "CategoryName";
-            this.CategoryName.Size = new System.Drawing.Size(270, 23);
+            this.CategoryName.Size = new System.Drawing.Size(308, 27);
             this.CategoryName.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(122, 102);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(227, 135);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 43);
+            this.button1.Size = new System.Drawing.Size(155, 57);
             this.button1.TabIndex = 3;
             this.button1.Text = "Add expense to this category";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(263, 103);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 42);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Add Category";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(10, 100);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(74, 133);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 44);
+            this.button3.Size = new System.Drawing.Size(122, 59);
             this.button3.TabIndex = 5;
             this.button3.Text = "Back";
             this.button3.UseVisualStyleBackColor = true;
@@ -98,16 +83,14 @@
             // 
             // Categories
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(407, 174);
+            this.ClientSize = new System.Drawing.Size(465, 232);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.CategoryName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Categories";
             this.Text = "Categories";
             this.Load += new System.EventHandler(this.Categories_Load);
@@ -122,7 +105,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox CategoryName;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
     }
 }
