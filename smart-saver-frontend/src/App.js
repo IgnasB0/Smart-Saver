38fc8288bc09b474ee9100d8bf873c7d709e86ca
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2020-12-20 15:54:30 +0200

    More wireframes

--- smart-saver-frontend/src/App.js
+++ smart-saver-frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MainForm from './MainForm';
+import ExpenseForm from './ExpenseForm';
 
 function App() {
   return (
     <div className="App">
       <MainForm/>
+      <ExpenseForm/> (/*Temporary location. Component spawn should be automized and in different window. This is just for representation.*/)
     </div>
   );
 }
---
+++ smart-saver-frontend/src/ExpenseComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ExpenseComponent.css';
+
+export default function ExpenseComponent() {
+    return (
+        <div class="component-box">
+            <p>*Component Wireframe*</p>
+            <button> Add Expense</button>
+        </div>
+    )
+}
